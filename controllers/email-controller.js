ce41b49579dce3829b1d3cd6fa67e1bb8ce752b3
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-11-24 15:51:27 +0300

    Add HTML and Event Details

--- controllers/email-controller.js
+++ controllers/email-controller.js
@@ -2,20 +2,35 @@ const transporter = require("../config/config");
 const eventDetails = require("../calendar/calendar.ical");
 exports.sendEmail = async (req, res) => {
   const info = await transporter.sendMail({
-    to: "john.brian@thejitu.com, njugunajohnbrian@gmail.com, caleb.baraka@thejitu.com, lucky.tsuma@thejitu.com, njugunajb96@gmail.com",
+    to: "njugunajohnbrian@gmail.com",
+    cc: " caleb.baraka@thejitu.com, lucky.tsuma@thejitu.com, njugunajb96@gmail.com, evans.kibet@thejitu.com",
     from: {
       name: "John Brian ",
       address: "njugunajb96@gmail.com",
     },
-    subject: "Testing Nodemailer",
-    // text: "Good Morning JB, This is just but a test email",
-    // // html: `<html><body><h4>ATTENTION!!</h4><p>INVITATION</p></body></html>`,
-    // // attachments: [
-    // //   {
-    // //     filename: "hello.txt",
-    // //     content: "Hey there",
-    // //   },
-    // // ],
+    subject: "MEET AND GREET",
+    html: `
+    <div>
+          <h4>Meet and Greet</h4>
+          <p>Why not take a break after a long day of looking at the screen?</p>
+          <br />
+          <p>I know your concur with me and that is why I present to you this event</p>
+          <br />
+          <p>Don't Miss!!</p>
+          <br />
+          <br />
+          <br />
+          <p>Please Ignore, It's a test email on the Nodemailer assignment</p>
+          <br />
+          <img src="cid:uniqueID" alt"alt"/>
+    <div/>`,
+    attachments: [
+      {
+        filename: "meetgreet.png",
+        path: "https://tinyurl.com/3xkyfcby",
+        cid: "uniqueID",
+      },
+    ],
     message: {
       headers: {
         "x-invite": {
